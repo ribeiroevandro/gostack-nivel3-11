d0a699eabadebc6f82b74258b96d6a091af0de4e
Deibson Cogo <deibsoncogo@outlook.com>
2020-08-13 21:33:54 -0300

    11.2 Tela de autenticação

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,30 +1,73 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import IconeFeather from 'react-native-vector-icons/Feather';
 
 import InputComponente from '../../components/input';
 import BotaoComponente from '../../components/botao';
-import { Container, Titulo } from './styles';
+import {
+	Container,
+	Titulo,
+	AjudaSenha,
+	AjudaSenhaTexto,
+	CriarConta,
+	CriarContaTexto,
+} from './styles';
 
 import logoImagem from '../../assets/logo.png';
 
 const Login: React.FC = () => {
 	return (
-		<Container>
-			<Image source={logoImagem} />
+		<>
+			<KeyboardAvoidingView
+				style={{ flex: 1 }}
+				// IDENTIFICA QUAL E O SISTEMA OPERACIONAL
+				behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+				enabled
+			>
+				<ScrollView
+					// FECHA O TECLADO AO CLICAR FORA DO CAMPO DE DIGITACAO
+					keyboardShouldPersistTaps="handled"
+					contentContainerStyle={{ flex: 1 }}
+				>
+					<Container>
+						<Image source={logoImagem} />
 
-			<Titulo>Faça seu login</Titulo>
+						<View>
+							<Titulo>Faça seu login</Titulo>
+						</View>
 
-			<InputComponente nome="email" icone="mail" placeholder="E-mail" />
-			<InputComponente nome="senha" icone="lock" placeholder="Senha" />
+						<InputComponente nome="email" icone="mail" placeholder="E-mail" />
+						<InputComponente nome="senha" icone="lock" placeholder="Senha" />
 
-			<BotaoComponente
+						<BotaoComponente
+							onPress={() => {
+								console.log('TE LIGUEI...');
+							}}
+						>
+							Entrar
+						</BotaoComponente>
+
+						<AjudaSenha
+							onPress={() => {
+								console.log('TOMO BANHO DE LUA...');
+							}}
+						>
+							<AjudaSenhaTexto>Esqueci minha senha</AjudaSenhaTexto>
+						</AjudaSenha>
+					</Container>
+				</ScrollView>
+			</KeyboardAvoidingView>
+
+			<CriarConta
 				onPress={() => {
-					console.log('TE LIGUEI...');
+					console.log('COMO UM ANJO...');
 				}}
 			>
-				Entrar
-			</BotaoComponente>
-		</Container>
+				<IconeFeather name="log-in" size={20} color="#ff9000" />
+
+				<CriarContaTexto>Criar uma conta</CriarContaTexto>
+			</CriarConta>
+		</>
 	);
 };
 
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,10 +1,11 @@
 import Styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = Styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-	padding: 0 30px;
+	padding: 0px 30px 50px 30px;
 `;
 
 export const Titulo = Styled.Text`
@@ -12,5 +13,39 @@ export const Titulo = Styled.Text`
   color: #f4ede8;
   /* DEFINE A FONTE PARA TODO O PROGRAMA */
   font-family: 'RobotoSlab-Medium';
-  margin: 64px 0 24px;
+  margin: 44px 0 14px;
+`;
+
+export const AjudaSenha = Styled.TouchableOpacity`
+	margin-top: 24px;
+`;
+
+export const AjudaSenhaTexto = Styled.Text`
+	color: #f4ede8;
+	font-size: 16px;
+	font-family: 'RobotoSlab-Medium';
+`;
+
+// TouchableWithoutFeedback FAZ NAO ACONTECER UM EFEITO AO CLICAR
+export const CriarConta = Styled.TouchableOpacity`
+	position: absolute;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background: #312e38;
+	border-top-width: 1px;
+	border-color: #232129;
+	/* VAI ADICIONAR O TAMANHO DE UM BOTAO QUE EXISTE EM BAIXO DO IPHONE COMO MARGEM */
+	padding: 16px 0 ${16 + getBottomSpace()}px;
+
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+`;
+
+export const CriarContaTexto = Styled.Text`
+	color: #ff9000;
+	font-size: 18px;
+	font-family: 'RobotoSlab-Medium';
+	margin-left: 16px;
 `;
